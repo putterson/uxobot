d1a09f1048c2a9293e0e84f0785863a41e99166d
Pat Goebel <putterson@gmail.com>
2013-07-23 00:24:17 -0400

    Finished genChildren and worked on minimax algo

--- uxobot.py
+++ uxobot.py
@@ -1,6 +1,14 @@
 #!/bin/python
 # coding=utf-8
 
+# A bot for the ultimate tic-tac-toe game described here
+# Utilizing alphabeta pruned minimax algorith at the moment
+# Make sure that your terminal supports utf-8 line drawing characters
+# 
+# TODO:
+#   add some way to store information on which boards have been won already
+#   
+#
 import sys
 
 class Game:
@@ -39,6 +47,7 @@ class Game:
                         print "Please enter a valid move."
                         continue
                 elif curplayer == 'O':
+                    print "Computer move."
                     self.lastmove = bot.move(state, self.lastmove, curplayer)
                 display.printBoard(state)
     
@@ -71,6 +80,11 @@ class Game:
             return False
 
 class XOHeuristic:
+    
+    depth = 10
+    colour = 'X'
+    lastmove = ()
+    
     wins = (
         (0,1,2),
         (3,4,5),
@@ -88,52 +102,93 @@ class XOHeuristic:
         (1000,  0,     0,     0),
         )
     
-    def move(self, state, lastmove, player):
-        # 
     
-    def genChildren(self, state, lastmove):
+    alpha = -1000
+    beta  =  1000
+    
+    def move(self, state, lastmove, player):
+        self.colour = player
+        moves = []
+        # call alphabeta with depth, it will return the end state and the list of moves leading to it
+        node = { 'state' : state, 'moves' : moves.append(lastmove)}
+        moves = self.alphabeta( node, self.depth, self.alpha, self.beta, player)
         
-        return { "state" : state, "children" : children }
-        
-    def evalPos( self, board, player ):
-        if player == 'X':
-            opponent = 'O'
-        else :
-            opponent = 'X'
+        # make best move by passing the chosen move up
+        return
+    
+    def genChildren(self, node):
+        if node['moves']:
+            lastmove = node['moves'].pop()
+        else:
+            lastmove = ()
+            node['moves'] = []
+        state = node['state']
+        moves = []
+        if len(lastmove) == 0:
+            for i,b in enumerate(state):
+                for j,c in enumerate(state[i]):
+                    if c == ' ':
+                        moves.append((i,j))
+        else:
+            i = lastmove[1]
+            for j,c in enumerate(state[i]):
+                if c == ' ':
+                    moves.append((i,j))
+            if len(moves) == 0:
+                for i,b in enumerate(state):
+                    for j,c in enumerate(state[i]):
+                        if c == ' ':
+                            moves.append((i,j))
+        node['moves'].append(lastmove)
+        return moves
+    
+    # need to fix this to give a correct score, workingish for now
+    def evalBoard(self, board, player):
+        t = 0
+        opponent = self.notPlayer(player)
         for i in range(8):
             players = 0
             others = 0
             for j in range(3):
-                piece = board[wins[i][j]]
+                piece = board[self.wins[i][j]]
                 if piece == player:
                     players += 1
                 elif piece == opponent:
                     others += 1
-            t += score[players][others]
+            t += self.score[players][others]
         return t
     
-    def evalState(self, node ):
+    # compute score for whole metaboard
+    def evalState(self, node, maximize):
         return 1000
     
+    # 
+    # 
     def alphabeta(self, node, depth, a, b, maximize):
-        if depth == 0 or len(node.children) == 0:
-            return evalState(node)
-        if maximize:
-            for child in node.children:
-                a = max(a, alphabeta(child, depth - 1, a, b, not maximize))
+        children = self.genChildren(node)
+        if depth == 0 or len(children) == 0:
+            return (evalState(node, maximize), state)
+        if maximize == self.colour:
+            for child in children:
+                node
+                comp = self.alphabeta(child, depth - 1, a, b, self.notPlayer(maximize))
+                a = max(a, comp[0])
                 if b <= a:
                     break #beta cutoff
-            return a
+            return (a, comp[1])
         else:
-            for child in node.children:
-                b = min(b, alphabeta(child, depth - 1, a, b, not maximize))
+            for child in children:
+                comp = self.alphabeta(child, depth - 1, a, b, self.notPlayer(maximize))
+                b = min(b, comp[0])
                 if b <= a:
                     break #alpha cutoff
-            return b
+            return (b, comp[1])
     
-    def minimax(self, node, depth, maximize):
-        if depth == 0 or len(node.children) == 0:
-            return
+    def notPlayer(self,p):
+        if p == 'X':
+            return 'O'
+        else:
+            return 'X'
 
 class GameState:
     e = [ ' ' for i in range (9) ]
